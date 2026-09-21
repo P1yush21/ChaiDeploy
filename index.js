f2ae4fa330d8e36b1c383943906beef92587931b
Piyush Sharma <piyushsharma21246@gmail.com>
2026-09-21 23:53:10 +0530

    github data added

--- index.js
+++ index.js
@@ -1,16 +1,60 @@
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const port = 3000
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = 3000;
 
-app.get('/',(req, res)=>{
-    res.send('Hello World')
+const github = {
+  login: "hiteshchoudhary",
+  id: 11613311,
+  node_id: "MDQ6VXNlcjExNjEzMzEx",
+  avatar_url: "https://avatars.githubusercontent.com/u/11613311?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/hiteshchoudhary",
+  html_url: "https://github.com/hiteshchoudhary",
+  followers_url: "https://api.github.com/users/hiteshchoudhary/followers",
+  following_url:
+    "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+  gists_url: "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+  subscriptions_url:
+    "https://api.github.com/users/hiteshchoudhary/subscriptions",
+  organizations_url: "https://api.github.com/users/hiteshchoudhary/orgs",
+  repos_url: "https://api.github.com/users/hiteshchoudhary/repos",
+  events_url: "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/hiteshchoudhary/received_events",
+  type: "User",
+  user_view_type: "public",
+  site_admin: false,
+  name: "Hitesh Choudhary",
+  company: null,
+  blog: "https://hitesh.ai",
+  location: "India",
+  email: null,
+  hireable: null,
+  bio: "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+  twitter_username: "hiteshdotcom",
+  public_repos: 127,
+  public_gists: 6,
+  followers: 60894,
+  following: 0,
+  created_at: "2015-03-23T13:03:25Z",
+  updated_at: "2026-09-02T16:52:30Z",
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Hi hello world </h1>");
+});
+
+app.get('/github',(req,res)=>{
+    res.send(github)
 })
 
-app.get('/login',(req, res)=>{
-    res.send('<h1>Hi hello world </h1>')
-})
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`Running of port : ${port}`);
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Running of port : ${port}`);
+});
